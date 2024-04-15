6ae3c88312ce076527bd0c248a0d35f228862cb7
luciegamby-sigmalabs <trainee.lucie.gamby@sigmalabs.co.uk>
2024-04-15 17:49:04 +0100

    Added docstrings

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -3,6 +3,12 @@ import requests
 
 
 def get_single_plant_data(plant_id: int) -> dict:
+    """
+    Contacts the api and uses the given plant_id to get to a
+    specific endpoint.
+    Returns the json data.
+    """
+
     response = requests.get(
         f"https://data-eng-plants-api.herokuapp.com/plants/{plant_id}")
 
