3ca6246cb9c4d6fa38cfccea0bac05634823da79
luciegamby-sigmalabs <trainee.lucie.gamby@sigmalabs.co.uk>
2024-04-16 17:03:33 +0100

    Splits botanist_name into botanist_first_name and botanist_last_name

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -3,9 +3,19 @@ from os import path
 
 import pandas as pd
 
+def extract_first_name_last_name(df: pd.DataFrame):
+    df[['botanist_first_name', 'botanist_last_name']]= df['botanist_name'].str.split(' ', expand=True)
+    first_name = df.pop("botanist_first_name")
+    last_name = df.pop("botanist_last_name")
+    df.insert(1, "botanist_first_name", first_name)
+    df.insert(2, "botanist_last_name", last_name)
+    df = df.drop('botanist_name', axis=1)
+    return df
+
 def clean_data(filename: str):
     if path.isfile(f"{filename}"):
         df = pd.read_csv(f"{filename}")
+        df = extract_first_name_last_name(df)
         df = df[df['soil_moisture'] > 0]
         df = df[df['soil_moisture'] < 100]
         df = df[df['temperature'] > 0]
