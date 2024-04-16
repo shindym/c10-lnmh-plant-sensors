b5d482a27c7925e61074c69c1fd91ed0a9bed4c9
luciegamby-sigmalabs <trainee.lucie.gamby@sigmalabs.co.uk>
2024-04-16 16:15:28 +0100

    Filtered out erroneous values

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,4 +1,16 @@
 """This file is responsible for cleaning and preparing plant data"""
+from os import path
+
+import pandas as pd
+
+def clean_data(filename: str):
+    if path.isfile(f"{filename}"):
+        df = pd.read_csv(f"{filename}")
+        df = df[df['soil_moisture'] > 0]
+        df = df[df['soil_moisture'] < 100]
+        df = df[df['temperature'] > 0]
+        df = df[df['temperature'] < 50]
+        df.to_csv(filename, index=False)
 
 if __name__ == "__main__":
-    pass
+    clean_data("data/plant_data.csv")
