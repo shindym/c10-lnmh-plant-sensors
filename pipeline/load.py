77c286fbbd766e906e9c7412ea4a2420e5da4076
Ariba <miss-syeda@outlook.com>
2024-04-17 17:01:51 +0100

    finish load functionality

--- pipeline/load.py
+++ pipeline/load.py
@@ -1,9 +1,18 @@
 """This file is responsible for loading the clean data into the database"""
 
-from os import environ
+from os import path, environ as ENV
 
 from dotenv import load_dotenv
 from pymssql import connect
+import pandas as pd
+
+INSERT_RECORDING_QUERY = """
+INSERT INTO 
+    s_delta.recordings (recording_taken, last_watered, soil_moisture, temperature, plant_id, botanist_id)
+VALUES 
+    (%s, %s, %s, %s, %s, %s);
+"""
+
 
 def get_db_connection(config):
     """
@@ -19,7 +28,34 @@ def get_db_connection(config):
         as_dict=True
     )
 
+
+def convert_to_list(filename):
+    """Converts a DataFrame to a list of tuples."""
+
+    if path.isfile(f"{filename}"):
+        df = pd.read_csv(filename)
+
+        return df.to_records(index=False).tolist() if not df.empty else None
+
+
+def load_data(conn, data):
+    """
+    Loads the data into the database.
+    """
+
+    with conn.cursor() as cur:
+
+        cur.executemany(INSERT_RECORDING_QUERY, data)
+
+    conn.commit()
+
+
 if __name__ == "__main__":
     load_dotenv()
-    
-    conn = get_db_connection(environ)
+
+    conn = get_db_connection(ENV)
+
+    data_to_upload = convert_to_list("data/clean_plant_data.csv")
+
+    if data_to_upload:
+        load_data(conn, data_to_upload)
