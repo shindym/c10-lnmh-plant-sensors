d1ec78d51bed53147c0a3a9d041be3dbfaf6df03
Ariba <miss-syeda@outlook.com>
2024-04-18 15:25:07 +0100

    added some more variables required by terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,6 +6,26 @@ variable "AWS_SECRET_ACCESS_KEY" {
   type = string
 }
 
+variable "DB_HOST" {
+  type = string
+}
+
+variable "DB_USER" {
+  type = string
+}
+
+variable "DB_PORT" {
+  type = string
+}
+
+variable "DB_NAME" {
+  type = string
+}
+
+variable "DB_PASSWORD" {
+  type = string
+}
+
 variable "DASHBOARD_IMAGE_URI" {
   type = string
 }
