547da12b44097a59092427e55e7f99981a376b47
luciegamby-sigmalabs <trainee.lucie.gamby@sigmalabs.co.uk>
2024-04-18 13:28:13 +0100

    Added send_alerts to pipeline script

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,6 +1,6 @@
 """This file is responsible for running the whole ETL pipeline."""
-from extract import get_all_plant_data,create_plant_csv
-from transform import clean_data,add_botanist_id
+from extract import get_all_plant_data, create_plant_csv
+from transform import clean_data, add_botanist_id, send_alerts
 from load import get_db_connection, convert_to_list, load_data
 from dotenv import load_dotenv
 from os import environ
@@ -16,12 +16,15 @@ def pipeline_process():
     conn = get_db_connection(environ)
     clean_data(f"{environ['storage_folder']}/plant_data.csv")
     add_botanist_id(f"{environ['storage_folder']}/plant_data.csv", conn)
-    # load 
+    send_alerts(conn)
+    # load
     conn = get_db_connection(environ)
-    data_to_upload = convert_to_list(f"{environ['storage_folder']}/clean_plant_data.csv")
+    data_to_upload = convert_to_list(
+        f"{environ['storage_folder']}/clean_plant_data.csv")
     if data_to_upload:
         load_data(conn, data_to_upload)
         conn.close()
+
 
 def handler(event: dict = None, context: dict = None) -> dict:
     """ logic from pipeline process into handler to be used in lambda."""
@@ -32,9 +35,10 @@ def handler(event: dict = None, context: dict = None) -> dict:
     conn = get_db_connection(environ)
     clean_data(f"{environ['storage_folder']}/plant_data.csv")
     add_botanist_id(f"{environ['storage_folder']}/plant_data.csv", conn)
-    # load 
+    # load
     conn = get_db_connection(environ)
-    data_to_upload = convert_to_list(f"{environ['storage_folder']}/clean_plant_data.csv")
+    data_to_upload = convert_to_list(
+        f"{environ['storage_folder']}/clean_plant_data.csv")
     if data_to_upload:
         load_data(conn, data_to_upload)
         conn.close()
