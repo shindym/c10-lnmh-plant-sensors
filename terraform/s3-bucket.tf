ffd692191578fca5f97f2fc0c728cfc8b6c6958c
Ariba <miss-syeda@outlook.com>
2024-04-19 13:57:16 +0100

    moved code to main and added policy related code to main

--- terraform/s3-bucket.tf
+++ terraform/s3-bucket.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region     = "eu-west-2"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-}
-
 resource "aws_s3_bucket" "long_term_storage_bucket" {
   bucket = "cretaceous-paleogene-terraform"
 
