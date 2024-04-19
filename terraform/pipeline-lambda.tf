c05ca467b375a49e6b6046caeaf0a48221dc05aa
Ariba <miss-syeda@outlook.com>
2024-04-19 13:58:01 +0100

    add pipeline lambda and transfer lambda

---
+++ terraform/pipeline-lambda.tf
@@ -0,0 +1,33 @@
+resource "aws_iam_role" "pipeline-lambda-role" {
+  name               = "c10-delta-pipeline-terraform-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda-role-policy.json
+}
+
+data "aws_ecr_repository" "pipeline-lambda-ecr-repo" {
+  name = "c10-delta-pipeline"
+}
+
+data "aws_ecr_image" "pipeline-lambda-image" {
+  repository_name = data.aws_ecr_repository.pipeline-lambda-ecr-repo.name
+  image_tag       = "latest"
+}
+
+resource "aws_lambda_function" "pipeline-lambda" {
+  role          = aws_iam_role.pipeline-lambda-role.arn
+  function_name = "c10-delta-pipeline-terraform-lambda"
+  package_type  = "Image"
+  image_uri     = data.aws_ecr_image.pipeline-lambda-image.image_uri
+  memory_size   = 150
+  timeout       = 300
+  environment {
+    variables = {
+      DB_HOST        = var.DB_HOST,
+      DB_NAME        = var.DB_NAME,
+      DB_USER        = var.DB_USER,
+      DB_PASSWORD    = var.DB_PASSWORD,
+      DB_PORT        = var.DB_PORT,
+      SCHEMA         = var.SCHEMA,
+      storage_folder = var.storage_folder
+    }
+  }
+}
