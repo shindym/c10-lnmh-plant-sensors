b7611b516e73f5d8f7c9803763f8463aee13b733
Ariba <miss-syeda@outlook.com>
2024-04-19 12:45:26 +0100

    terraforming s3 bucket and updating variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,31 +5,3 @@ variable "AWS_ACCESS_KEY_ID" {
 variable "AWS_SECRET_ACCESS_KEY" {
   type = string
 }
-
-variable "DB_HOST" {
-  type = string
-}
-
-variable "DB_USER" {
-  type = string
-}
-
-variable "DB_PORT" {
-  type = string
-}
-
-variable "DB_NAME" {
-  type = string
-}
-
-variable "DB_PASSWORD" {
-  type = string
-}
-
-variable "DASHBOARD_IMAGE_URI" {
-  type = string
-}
-
-variable "PIPELINE_IMAGE_URI" {
-  type = string
-}
