2c4f745d8ad361e00b5cb09a7ce0ecf4f6a495db
Ariba <miss-syeda@outlook.com>
2024-04-19 13:58:20 +0100

    add schedule for pipeline and bug fix

---
+++ terraform/pipeline-schedule.tf
@@ -0,0 +1,38 @@
+data "aws_iam_policy_document" "pipeline-schedule-permissions-policy" {
+
+  statement {
+    effect = "Allow"
+
+    resources = [
+      aws_lambda_function.pipeline-lambda.arn,
+      "${aws_lambda_function.pipeline-lambda.arn}:*"
+    ]
+
+    actions = ["lambda:InvokeFunction"]
+  }
+}
+
+resource "aws_iam_role" "pipeline-schedule-role" {
+  name               = "c10-delta-schedule-role-terraform"
+  assume_role_policy = data.aws_iam_policy_document.schedule-trust-policy.json
+
+  inline_policy {
+    name   = "c10-delta-inline-lambda-execution-policy"
+    policy = data.aws_iam_policy_document.pipeline-schedule-permissions-policy.json
+  }
+}
+
+resource "aws_scheduler_schedule" "pipeline-schedule" {
+  name                = "c10-delta-pipeline-schedule-terraform"
+  group_name          = "default"
+  schedule_expression = "cron(*/1 * * * ? *)"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  target {
+    arn      = aws_lambda_function.pipeline-lambda.arn
+    role_arn = aws_iam_role.pipeline-schedule-role.arn
+  }
+}
